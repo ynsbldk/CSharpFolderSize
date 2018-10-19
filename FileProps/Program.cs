6678907c26b577d7279f1dc3df12a6ea0386071b
Yunus BULDUK <y_bulduk@aof.anadolu.edu.tr>
2018-10-19 22:02:48 +0300

    Add project files.

---
+++ FileProps/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileProps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dinfo = new DirectoryInfo(@"D:\Media\English");
+            Console.WriteLine(dinfo.GetTotalSize());
+            Console.ReadLine();
+            
+
+        }
+
+    }
+    ///// Extensions
+    public static class DirectoryExtensions
+    {
+        public static long GetTotalSize(this DirectoryInfo Directory)
+        {
+            long result = 0;
+            try
+            {
+                foreach (var file in Directory.GetFiles())
+                    result += file.Length;
+                foreach (var dir in Directory.GetDirectories())
+                    result += GetTotalSize(dir);
+            }
+            catch (Exception e)
+            {
+
+            }
+            return result;
+        }
+    }
+}
